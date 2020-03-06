8e98036a3d84e4fe88da09564b563a1e628f4735
Maxwell Hankner <maxwellhankner@gmail.com>
2020-03-05 20:52:39 -0600

    fixed a non number bug, finished comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,8 +6,8 @@ function writePassword() {
   // Ask the user for requirements and asign them to variables
   // Ask for the password length
   var passLength = prompt('Password length? (8-128)');
-  // If the length is shorter that 8, or longer then 128, ask again
-  if (passLength > 128 || passLength < 8){
+  // If the length is shorter that 8, or longer then 128, on not a number, ask again
+  if (passLength > 128 || passLength < 8 || typeof passLength !== 'number'){
     alert('Try again, pick a length between 8 and 128.');
     return;
   }
@@ -19,33 +19,33 @@ function writePassword() {
   var includeNumbers = confirm('Include numbers?');
   // Ask if the user would like to include symbols
   var includeSymbols = confirm('Include symbols?');
-
+  // Check to see the user included at least one type of symbol
   if (includeUppers === false && includeLowers === false && includeSymbols === false && includeNumbers === false) {
     alert('Try again, you must include one type of character.');
     return;
   } 
-
-  // Run the generate password function with the requrements and length as perameters
+  // Create password variable
   var password;
-
+  // Create password is a verified password variable
   var passwordGood = true;
   while (passwordGood) {
+    // Generate a password and set it to the password variable
     password = generatePassword(passLength, includeUppers, includeLowers, includeNumbers, includeSymbols);
+    // If the password is good, then break the loop, if not, generate another one
     if (password !== 'TRY AGAIN') {
       passwordGood = false;
     }
   }
-
   // connect the password area to a variable
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// Add event listener to generate button, when it is clicked, run the writePassword function
+// Add an event listener to the generate button, when it is clicked, run the writePassword function
 generateBtn.addEventListener("click", writePassword);
 
-// Generator functions
+// Random character generator functions
 // Generate a random lowercase character
 function randomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -58,39 +58,37 @@ function randomUpper() {
 function randomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-// Generate a symbol character
+// Generate a random symbol character
 function randomSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,.';
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
-
-// Password generator
+// Password generator function
 function generatePassword(passLength, includeUppers, includeLowers, includeNumbers, includeSymbols) {
-
   // Create a variable for the final password
   var finalPassword = '';
-
-  // Create an array of the generating character functions that the user required
+  // Create an array of the random character generator functions that the user required
   var requiredFunctions = []
-  // Iclude the randomUpper function
+  // Include the randomUpper function to the array
   if (includeUppers) {
     requiredFunctions.push(randomUpper);
   }
-  // Iclude the randomLower function
+  // Include the randomLower function to the array
   if (includeLowers) {
     requiredFunctions.push(randomLower);
   }
-  // Iclude the randomBumber function
+  // Include the randomBumber function to the array
   if (includeNumbers) {
     requiredFunctions.push(randomNumber);
   }
-  // Iclude the randomSymbol function
+  // Include the randomSymbol function to the array
   if (includeSymbols) {
     requiredFunctions.push(randomSymbol);
   }
 
   // Generate the desired amount of random characters by running the character generator functions randomy
+  // Note: there is a chance that a required character generator doesnt get used, so we just run this whole thing again in the writePassword function
   for (var i = 0; i < passLength; i++) {
     // Get a random number between 0 and 3
     var randomFunctionIndex = Math.floor(Math.random() * requiredFunctions.length);
@@ -101,44 +99,51 @@ function generatePassword(passLength, includeUppers, includeLowers, includeNumbe
   }
 
   // Check to see if at least one of each character type was generated in the result
+  // Strings for all the possible characters seperated by type
   var possibleLowercase = 'abcdefghijklmnopqrstuvwxyz';
   var possibleUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var possibleNumbers = '1234567890';
   var possibleSymbols = '!@#$%^&*(){}[]=<>/,.';
 
+  // Create boolean variables for each type of character
   var hasLower = false;
   var hasUpper = false;
   var hasNumber = false;
   var hasSymbol = false;
 
+  // Loop every character of our generated password and check if it matches the possible required types
   for (var i = 0; i < passLength; i++) {
     for (var x = 0; x < possibleLowercase.length; x++) {
       if (finalPassword[i] === possibleLowercase[x]) {
+        // If the current character matches any lowercase character, change hasLower to true
         hasLower = true;
       }
     }
     for (var x = 0; x < possibleUppercase.length; x++) {
       if (finalPassword[i] === possibleUppercase[x]) {
+        // If the current character matches any uppercase character, change hasUpper to true
         hasUpper = true;
       }
     }
     for (var x = 0; x < possibleNumbers.length; x++) {
       if (finalPassword[i] === possibleNumbers[x]) {
+        // If the current character matches any number character, change hasNumber to true
         hasNumber = true;
       }
     }
     for (var x = 0; x < possibleSymbols.length; x++) {
       if (finalPassword[i] === possibleSymbols[x]) {
+        // If the current character matches any symbol character, change hasSymbol to true
         hasSymbol = true;
       }
     }
   }
   
-  // Return the password result
-  if(includeUppers === hasUpper && includeLowers === hasLower && includeNumbers === hasNumber && includeSymbols === hasSymbol) {
+  // Return the password result if all requirements are met
+  if (includeUppers === hasUpper && includeLowers === hasLower && includeNumbers === hasNumber && includeSymbols === hasSymbol) {
     return finalPassword;
   }
 
-  // If the password does not meet requirements, then return try again
+  // If the password does not meet requirements, then return 'try again'
   return 'TRY AGAIN';
 }
